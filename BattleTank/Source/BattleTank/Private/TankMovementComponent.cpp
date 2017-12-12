8c5c63bae3b0aa306053c212161e9c8c37d83534
DESKTOP-FHH5O0R\Trypants <anthonystonge10@gmail.com>
2017-12-11 22:45:36 -0500

    Movement with turn "Right" function

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,14 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
-
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet){ return; }
+	RightTrack = RightTrackToSet;
+	LeftTrack = LeftTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	//auto Time = GetWorld()->GetTimeSeconds()();
-	auto Name = GetName();
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+}
 
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw : %f"), Throw);
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 }
--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -6,10 +6,6 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	
-
-	//TODO Clamp actual throttle value. 
-
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
